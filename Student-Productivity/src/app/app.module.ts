0ecff6d5bdb70274b9bd9d784737c77c3b20322d
Sara M <smehdinia@rollins.edu>
2022-03-06 17:20:41 -0500

    downloaded ngx-countdown library, integrated timer-itself component into appmodule

--- Student-Productivity/src/app/app.module.ts
+++ Student-Productivity/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CountdownModule } from 'ngx-countdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TimerItselfComponent} from './timer/timer-itself/timer-itself.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerItselfComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CountdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ Student-Productivity/src/app/timer/timer-itself/timer-itself.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-timer-itself',
+  templateUrl: './timer-itself.component.html'
+
+
+})
+
+export class TimerItselfComponent{
+
+}
+
+
