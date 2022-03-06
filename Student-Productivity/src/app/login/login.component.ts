9cd739842ddd32786e879b90cf25aa21085cd92c
Zgould24 <zgould@rollins.edu>
2022-03-06 01:57:47 -0500

    Add more styling and updated README file

--- Student-Productivity/src/app/login/login.component.ts
+++ Student-Productivity/src/app/login/login.component.ts
@@ -15,49 +15,44 @@ export class LoginComponent implements OnInit {
   }
 
   //Allows user to navigate to the registration form from the login screen
-  goToCreateAccount() {
+  goToCreateAccount(e:Event) {
     let loginRef = document.getElementById("login");
     let createRef = document.getElementById("create-account");
 
-    document.addEventListener("click", e => {
-      e.preventDefault();
-      loginRef?.classList.add("hide-login-container");
-      createRef?.classList.remove("hide-create-account");
-    })
+    loginRef?.classList.add("hide-login-container");
+    createRef?.classList.remove("hide-create-account");
   }
 
   //Allows user to navigate back to login screen if registration isn't necessary
-  goToLogin() {
+  goToLogin(e: Event) {
     let loginRef = document.getElementById("login");
     let createRef = document.getElementById("create-account");
 
-    document.addEventListener("click", e => {
-      e.preventDefault();
-      loginRef?.classList.remove("hide-login-container");
-      createRef?.classList.add("hide-create-account");
-    })
+    e.preventDefault();
+    loginRef?.classList.remove("hide-login-container");
+    createRef?.classList.add("hide-create-account");
   }
 
   //Displays an error to the user stating their username/password combination is invalid
   //
   //DEFAULT VALUES FOR LOGIN --> username = "0" password = "0"
   //Error will display if the user has entered nothing into the username and password boxes
-  showLoginError() {
+  showLoginError(e: Event) {
     let userInfo = (<HTMLInputElement>document.getElementById("user-info"));
     let passInfo = (<HTMLInputElement>document.getElementById("password-info"));
     let error = document.getElementById("login-error");
+
+    e.preventDefault();
+  
+    if(userInfo.value === '0' && passInfo.value === "0") {
+      error?.classList.add("hide-login-error");
+      userInfo.style.borderColor = "rgb(141, 141, 141)";
+      passInfo.style.borderColor = "rgb(141, 141, 141)";
+    }  else {
+      error?.classList.remove("hide-login-error"); 
+      userInfo.style.borderColor = "red";
+      passInfo.style.borderColor = "red";
+    }
     
-    document.addEventListener("click", e => {
-      e.preventDefault();
-      if(userInfo.value === '0' && passInfo.value === "0") {
-        error?.classList.add("hide-login-error");
-        userInfo.style.borderColor = "black";
-        passInfo.style.borderColor = "black";
-      }  else {
-        error?.classList.remove("hide-login-error"); 
-        userInfo.style.borderColor = "red";
-        passInfo.style.borderColor = "red";
-      }
-    })
   }
 }
