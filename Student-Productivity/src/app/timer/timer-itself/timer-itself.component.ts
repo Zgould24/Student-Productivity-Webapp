9e8ce1c59bbbabc5c1d6095bea788c6c92465fb2
Sara M <smehdinia@rollins.edu>
2022-03-07 01:42:46 -0500

    users can now put in hours minutes and seconds instead of just seconds only

--- Student-Productivity/src/app/timer/timer-itself/timer-itself.component.ts
+++ Student-Productivity/src/app/timer/timer-itself/timer-itself.component.ts
@@ -10,9 +10,21 @@ import { Component } from "@angular/core";
 export class TimerItselfComponent{
 
   newTime = 0;
+  newHours = 3;
+  newMinutes=4;
+  newSeconds=55;
+  overallTime=0;
+
   onSetTimer(newTimeInput: HTMLTextAreaElement) {
     this.newTime= parseInt(newTimeInput.value);
   }
+  onSetTimer2(newHoursInput: HTMLTextAreaElement, newMinutesInput: HTMLTextAreaElement, newSecondsInput: HTMLTextAreaElement) {
+    this.newHours= 3600 * parseInt(newHoursInput.value);
+    this.newMinutes=60 * parseInt(newMinutesInput.value);
+    this.newSeconds=parseInt(newSecondsInput.value);
+    this.overallTime= this.newHours + this.newMinutes + this.newSeconds;
+  }
+
 }
 
 
