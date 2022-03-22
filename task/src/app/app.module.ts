e5f49df4d16fe623afd622741faf7125314f6351
Britley Desir <desir.britley@gmail.com>
2022-03-21 20:41:51 -0400

    added navbar and assignment components

--- task/src/app/app.module.ts
+++ task/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { AssignmentComponent } from './assignment/assignment.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AssignmentComponent
+    AssignmentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
