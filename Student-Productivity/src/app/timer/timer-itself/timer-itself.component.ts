a013fe04a47ac10e1916efd6e6aa74b4c67c52cb
Sara M <smehdinia@rollins.edu>
2022-03-23 00:11:15 -0400

    restricted values user can enter for hours, minutes, and seconds, and added error messages for when user inputs anything outside of integers

--- Student-Productivity/src/app/timer/timer-itself/timer-itself.component.ts
+++ Student-Productivity/src/app/timer/timer-itself/timer-itself.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+/*import { isNumeric } from 'rxjs/util/isNumeric';
+*/
 
 @Component({
   selector: 'app-timer-itself',
@@ -10,19 +12,47 @@ import { Component } from "@angular/core";
 export class TimerItselfComponent{
 
   newTime = 0;
-  newHours = 3;
-  newMinutes=4;
-  newSeconds=55;
+  newHours = 0;
+  newMinutes= 0;
+  newSeconds= 0;
   overallTime=0;
+  newError='no error';
 
   onSetTimer(newTimeInput: HTMLTextAreaElement) {
     this.newTime= parseInt(newTimeInput.value);
   }
   onSetTimer2(newHoursInput: HTMLTextAreaElement, newMinutesInput: HTMLTextAreaElement, newSecondsInput: HTMLTextAreaElement) {
+    /*check value and make sure it is not invalid
+    */
+
+   if (isNaN(parseInt(newHoursInput.value)) == true) {
+      /*error message*/
+
+      this.newError = 'must enter valid integer';
+      return;
+
+    /*document.getElementById('newError')!.innerHTML = 'STOP';*/
+    }
+
+  /*this.newError = 'STOP';
+    /*
+    do the num restriction
+    */
+    if (parseInt(newMinutesInput.value) > 60) {
+      newMinutesInput.value = '60';
+    }
+    if (parseInt(newSecondsInput.value) > 60) {
+      newSecondsInput.value = '60';
+    }
+
+
     this.newHours= 3600 * parseInt(newHoursInput.value);
     this.newMinutes=60 * parseInt(newMinutesInput.value);
     this.newSeconds=parseInt(newSecondsInput.value);
     this.overallTime= this.newHours + this.newMinutes + this.newSeconds;
+    if (this.overallTime > 86399) {
+      this.overallTime = 86399;
+    }
     this.newHours= 0;
     this.newMinutes= 0;
     this.newSeconds= 0;
