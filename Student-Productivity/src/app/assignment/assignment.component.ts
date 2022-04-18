5c44cb710c90bf491e35db022d1ae718f7ca21b7
Britley Desir <desir.britley@gmail.com>
2022-04-18 06:27:45 -0400

    Cleaned up code and added comments

--- Student-Productivity/src/app/assignment/assignment.component.ts
+++ Student-Productivity/src/app/assignment/assignment.component.ts
@@ -7,22 +7,27 @@ import { Assignment } from '../models/assignment';
   styleUrls: ['./assignment.component.css']
 })
 export class AssignmentComponent{
+  // Vairable declarations
 
   assignment: Assignment;
   homeworks: any;
   completes: any;
   clicked = false;
+
+  // Utlizing constructor defined in models folder
   constructor() { 
     this.assignment = new Assignment();
   }
 
+  // To do assignments and completed assignments being pushed to array
+  // to be saved for later use
   ngOnInit(): void {
-
     this.homeworks = [];
     this.completes = [];
     console.log(this.homeworks);
   }
 
+  // Creates an assignment and pushes it to the homework array
   submit() {
     let newAssignment = new Assignment(this.assignment.course, this.assignment.assignment, this.assignment.dueDate);
     this.assignment = newAssignment;
@@ -31,15 +36,11 @@ export class AssignmentComponent{
     this.clicked = true;
   }
 
+  // Moves an assignment from the upcoming section to the
+  // completed section
   complete(event: any) {
     console.log(event);
     this.completes.push(event);
     this.homeworks.splice(this.homeworks.indexOf(event), 1);
   }
-
-  // clear() {
-  //   this.assignment.course = '';
-  //   this.assignment.assignment = '';
-  //   this.assignment.dueDate = '';
-  // }
 }
