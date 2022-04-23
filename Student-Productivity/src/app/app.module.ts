96ed6db2a86bfa37d65607e88ea8d416f5505312
Hiroki Sato <satouhiroshiki@satouhiikinoair.rollins.edu>
2022-04-23 15:07:07 -0400

    Commiting changes on my personal branch

--- Student-Productivity/src/app/app.module.ts
+++ Student-Productivity/src/app/app.module.ts
@@ -1,16 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
-import { AssignmentComponent } from './assignment/assignment.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CourseCreateComponent } from './courses/course-create/course-create.component';
-import { CourseListComponent } from './courses/course-list/course-list.component';
-
-=======
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Router, Routes } from '@angular/router';
@@ -26,37 +15,22 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent}
 ];
->>>>>>> a7762723f96a7a0a52ae7ea57bfbe01ab0616cf7
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    AssignmentComponent,
-    NavbarComponent,
-    CourseCreateComponent,
-    CourseListComponent,
-
-=======
     LoginComponent,
     HomeComponent,
     HeaderComponent
->>>>>>> a7762723f96a7a0a52ae7ea57bfbe01ab0616cf7
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
-=======
     FormsModule,
     HttpClientModule
   ],
   providers: [
     LoginService
->>>>>>> a7762723f96a7a0a52ae7ea57bfbe01ab0616cf7
   ],
   bootstrap: [AppComponent]
 })
