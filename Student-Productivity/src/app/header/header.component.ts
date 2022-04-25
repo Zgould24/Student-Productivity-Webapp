94977d01efab599cade5318ba10e46124feb5122
Hiroki Sato <satouhiroshiki@satouhiikinoair.rollins.edu>
2022-04-25 16:35:41 -0400

    commit to fix a header bug

--- Student-Productivity/src/app/header/header.component.ts
+++ Student-Productivity/src/app/header/header.component.ts
@@ -1,26 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import * as e from 'express';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userIsAuthenticated: boolean;
+  private authenticationSubscription:Subscription;
 
-  constructor(private router:Router) { }
-
+  constructor(private authService: AuthService) { }
   ngOnInit(): void {
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authenticationSubscription = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+      console.log(this.userIsAuthenticated);
+    });
   }
 
-  goToLoginPage() {
-    this.router.navigateByUrl('/login');
+  ngOnDestroy(){
+  }
+
+  onLogOut() {
+    this.authService.logOut();
+  }
+  getUserIsAuthenticated (){
+    return this.userIsAuthenticated;
   }
 
   displayOptions() {
     let logout = <HTMLInputElement> document.getElementById("options");
-
     if(logout.classList.contains("options")) {
       logout.classList.remove("options");
       logout.classList.add("hide");
@@ -29,5 +39,4 @@ export class HeaderComponent implements OnInit {
       logout.classList.add("options");
     }
   }
-
 }
