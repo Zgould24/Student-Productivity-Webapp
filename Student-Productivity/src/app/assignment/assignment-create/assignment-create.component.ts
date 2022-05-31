9c502301565dbc0e063f0af7bdbeaa1fdfe947d9
Hiroki Sato <hsato@rollins.edu>
2022-05-31 17:16:12 -0400

    updating the assignment create form

--- Student-Productivity/src/app/assignment/assignment-create/assignment-create.component.ts
+++ Student-Productivity/src/app/assignment/assignment-create/assignment-create.component.ts
@@ -4,6 +4,8 @@ import { AssignmentsService } from '../assignments.service';
 import { CoursesService } from '../../courses/courses.service';
 import {Course} from '../../models/course';
 import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {FormsModule,ReactiveFormsModule, Validators} from '@angular/forms';
 @Component({
   selector: 'app-assignment-create',
   templateUrl: './assignment-create.component.html',
@@ -12,12 +14,15 @@ import { Subscription } from 'rxjs';
 export class AssignmentCreateComponent implements OnInit, OnDestroy{
 
   assignment: Assignment;
+  assignmentForm: FormGroup;
  
   private courseSub: Subscription;
   private courseNames: string[];
   private NoCourses: boolean;
   
-  constructor(private assignmentService: AssignmentsService, private courseService: CoursesService) { 
+  constructor(private assignmentService: AssignmentsService, 
+              private courseService: CoursesService,
+              private fb: FormBuilder) { 
     this.assignment = new Assignment();
     this.courseNames = [];
   }
@@ -39,6 +44,12 @@ export class AssignmentCreateComponent implements OnInit, OnDestroy{
       }
       }
     });
+
+    this.assignmentForm = this.fb.group({
+      courseName: ['', Validators.required],
+      assignmentName: ['', Validators.required],
+      dueDate: ['', Validators.required]
+    });
     //  console.log(this.courseNames);
   }
   ngOnDestroy(){
@@ -49,9 +60,11 @@ export class AssignmentCreateComponent implements OnInit, OnDestroy{
     return this.courseNames;
   }
   submit() {
-    const courseName = this.assignment.course || "INVDINPUT";
-    const assignmentName = this.assignment.assignment || "INVDINPUT";
-    const dueDate = this.assignment.dueDate || "INVDINPUT";
+
+    const courseName = this.assignmentForm.controls['courseName'].value || "INVDINPUT";
+    const assignmentName = this.assignmentForm.controls['assignmentName'].value || "INVDINPUT";
+    const dueDate = this.assignmentForm.controls['dueDate'].value || "INVDINPUT";
+    console.log(courseName);
     // let newAssignment = new Assignment(courseName, assignmentName, dueDate);
     if ( courseName === "INVDINPUT" || assignmentName === "INVDINPUT" || dueDate === "INVDINPUT") {
       return ;
